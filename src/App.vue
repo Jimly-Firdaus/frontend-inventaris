b643d3b3ed91b56b5b04b7ecaf5a880ce00b17ef
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2025-03-30 20:54:01 +0700

    chore: init tailwind, purge typescript

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,5 @@
 </template>
 
 <script setup lang="ts">
-//
+import "./index.css";
 </script>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.vue", "./src/**/*.ts"],
+  theme: {
+    extend: {}
+  },
+  plugins: [],
+  prefix: "tw-",
+}
