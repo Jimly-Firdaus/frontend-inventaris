9f195a1333b15c93473a951489acb9c1797fed7e
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2025-04-03 22:55:29 +0700

    fix: eslint for promise

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -26,7 +26,7 @@ api.interceptors.request.use(
     }
     return config;
   },
-  (error) => Promise.reject(error)
+  (error) => Promise.reject(error instanceof Error ? error : new Error(String(error)))
 );
 
 export default defineBoot(({ app }) => {
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -12,7 +12,7 @@ const formData = ref({
 const isPwd = ref(true);
 
 const onLogin = async () => {
-  store.auth.login(formData.value);
+  await store.auth.login(formData.value);
 
   if (store.auth.user) {
     switch (store.auth.user.role) {
