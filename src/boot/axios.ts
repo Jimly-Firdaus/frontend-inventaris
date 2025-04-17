b174a6da7d6b8dd53c38be3551e3acec9072c687
Jimly Firdaus <101716223+Jimly-Firdaus@users.noreply.github.com>
2025-04-17 21:10:39 +0700

    fix: add domain name

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -16,7 +16,7 @@ declare module 'vue' {
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: "https://api.tokohello/api" });
+const api = axios.create({ baseURL: "https://api.tokohello.com/api" });
 
 api.interceptors.request.use(
   (config) => {
